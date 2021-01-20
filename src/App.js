a6502c4d0c7ad4d708b23b1ddf4354b8080a8895
Tum <tum.suphakorn@gmail.com>
2021-01-20 16:16:18 +0700

    fix base url

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename="/games-db-api-project">
         <NavBar />
         <Route path="/" exact component = { GamePage } />
         <Route path="/page/:page" render={(props) => <GamePage {...props} key={Date.now()} />} />
